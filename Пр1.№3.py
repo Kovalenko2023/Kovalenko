d3663ce93617bccacd0163c6a69d4e880408ab8a
Kovalenko2023 <151244185+Kovalenko2023@users.noreply.github.com>
2023-11-24 23:19:59 +0200

    Add files via upload

---
+++ Пр1.№3.py
@@ -0,0 +1,7 @@
+t = input("Введіть символ: ")
+
+print(f"{t}{t}{t}{t}")
+print(f"{t}")
+print(f"{t}")
+print(f"{t}")
+print(f"{t}")
---
+++ Пр3. №1.py
@@ -0,0 +1,7 @@
+n = int(input("Напишіть ціле додатнє число!"))
+f=1
+for i in range(2, n+1):
+    f=f*i
+print(f)
+
+    
